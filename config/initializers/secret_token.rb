8001b383b76c56b9e38af92525f06f086e4b26b0
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2011-04-15 16:49:05 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoatParty::Application.config.secret_token = 'faf638d74ee1613e4fd9bff75e746524bb7b87af88913408f7b8354e1ee93dbd4f1aab781f2f9f6688d74907d8fbe3bd35e7df7af47eb4f761fe9ecbf9c2dda2'
